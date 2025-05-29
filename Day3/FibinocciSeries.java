ffc9428d4145c17299efe626c3a4132a1c2d4f64
Athira <athiravenugopal24@gmail.com>
2025-05-29 11:11:01 +0530

    Basic java programs

---
+++ Day3/FibinocciSeries.java
@@ -0,0 +1,14 @@
+class FibinocciSeries
+{
+public static void main(String args[])
+{
+int n1=0;
+int n2=1;
+int n3=n2+n1;
+int i;
+if(i=2;i>n;i++)
+{
+n1=n2;
+n2=n3;
+}
+}
---
+++ Day3/LeftStarPattern.java
@@ -0,0 +1,23 @@
+class  LeftStarPattern
+{
+public static void main(String args[])
+{
+int i;
+int j;
+int row=4;
+
+for (i=0;i<row;i++)
+{
+for (j=2*(row-i);j>=0;i++)
+{
+for (j=0;j<=i;j++)
+{
+System.out.println("*");
+{
+System.out.println();
+}
+}
+}
+}
+}
+}
