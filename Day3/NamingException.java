2b714f46d8c51f26af3c59a29c3800433885a757
Athira <athiravenugopal24@gmail.com>
2025-06-03 16:20:06 +0530

    day3

---
+++ Day3/NamingException.java
@@ -0,0 +1,19 @@
+class NamingException 
+{
+public static void main(String args[])
+{
+String name = "Alex";
+if (name.length()<4)
+{
+System.out.println("Name is not allowed");
+}
+else 
+{
+System.out.println("Name is allowed");
+}
+}
+}
+
+
+
+
