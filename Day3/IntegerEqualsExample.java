cb23e82c128d4948609b90f93f8b563868f3c986
Athira <athiravenugopal24@gmail.com>
2025-06-10 14:22:16 +0530

    Day3

---
+++ Day3/IntegerEqualsExample.java
@@ -0,0 +1,19 @@
+public class IntegerEqualsExample 
+
+{
+    public static void main(String[] args)
+
+	{
+        Integer i1 = 10;    
+        Integer i2 = 10;   
+
+        Integer i3 = 128;  
+        Integer i4 = 128;  
+
+        System.out.println("i1 == i2: " + (i1 == i2));  
+        System.out.println("i3 == i4: "  +(i3.equals(i4)));
+		
+
+        
+    }
+}
