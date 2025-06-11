a29cfeee92b693c5115cd70f61a1aec5c26b7af5
Athira <athiravenugopal24@gmail.com>
2025-06-11 12:42:40 +0530

    Day

---
+++ Day3/ArrayListExample.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ArrayListExample
+ {
+    public static void main(String[] args) 
+	{
+        ArrayList<String> names = new ArrayList<>();
+
+        names.add("Alice");
+        names.add("Bob");
+        names.add("Charlie");
+
+        System.out.println("Names in ArrayList:" +names);
+		}
+		}
+		
