306d7e83fcb655205591dc934c799151a40e9905
Athira <athiravenugopal24@gmail.com>
2025-06-12 13:36:43 +0530

    Day3

---
+++ Day3/VectorExample.java
@@ -0,0 +1,14 @@
+import java.util.Vector;
+class VectorExample 
+{
+public static void main (String args[])
+{
+Vector<String> v1 = new Vector<String>();
+v1.add("dog");
+v1.add("cat");
+v1.add("cow");
+v1.add("dog");
+System.out.println("Vectorlist:"  +v1);
+
+}
+}
